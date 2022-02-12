117d398b890af496a2039fc59c59ba488cd9bb80
Adriano M. Martins <22916477+adrianomqsmts@users.noreply.github.com>
2022-02-12 17:54:20 -0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Container, Badge } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch('https://api.github.com/users/adrianomqsmts/repos');
+      const data = await response.json();
+      setRepositories(data);
+    })();
+  }, []);
+
+  const updatedAt = (d) => {
+    var timeDiff = Math.abs((new Date()) - (new Date(d)));
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays
+  }
+
+  return (
+    <Container>
+      {repositories
+        .sort(({ pushed_at: pre }, { pushed_at: cur }) => updatedAt(pre) - updatedAt(cur))
+        .map((repository) => (
+          <Card key={repository.id} className='mb-3'>
+            <Card.Body>
+              <Card.Title className='display-4 text-center'>
+                {repository.name}
+              </Card.Title>
+              <Card.Text>
+                {repository.language &&
+                  <Badge bg="primary" className='h6'>{repository.language}</Badge>
+                }{repository.topics &&
+                  repository.topics.map((topic) => (
+                    <Badge bg="success" className='h6'> {topic} </Badge>
+                  ))
+                }
+              </Card.Text>
+              <hr />
+              <Card.Text>
+                Descrição do projeto: {repository.description} |  |  | {repository.html_url}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted text-center">Updated {updatedAt(repository.pushed_at)} days ago</Card.Footer>
+          </Card>
+        ))}
+    </Container>
+  );
+
+}
+
+
+export default App;
